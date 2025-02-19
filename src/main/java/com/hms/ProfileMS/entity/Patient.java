1faf1ab879a88d17caad408b549c5b770342554a
skbnsl <bansalsagar13@gmail.com>
2025-02-19 18:26:49 +0530

    add column in patient

--- src/main/java/com/hms/ProfileMS/entity/Patient.java
+++ src/main/java/com/hms/ProfileMS/entity/Patient.java
@@ -42,9 +42,12 @@ public class Patient {
     @Column(unique = true)
     private String aadharNo;
     private BloodGroup bloodGroup;
+    private String allergies;
+    private String chronicDisease;
 
     public PatientDTO toDTO(){
-        return new PatientDTO(this.id,this.name,this.email,this.dob,this.phone,this.address,this.aadharNo,this.bloodGroup);
+        return new PatientDTO(this.id,this.name,this.email,this.dob,this.phone,
+            this.address,this.aadharNo,this.bloodGroup,this.allergies,this.chronicDisease);
     }
 
 }
