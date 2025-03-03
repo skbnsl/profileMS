22e7d34a563313d511b7d2aef69b5a45248ba9cf
skbnsl <bansalsagar13@gmail.com>
2025-03-03 15:10:44 +0530

    issue resolve where doctor is not creating

--- src/main/java/com/hms/ProfileMS/repository/DoctorRepository.java
+++ src/main/java/com/hms/ProfileMS/repository/DoctorRepository.java
@@ -5,11 +5,10 @@ import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 import com.hms.ProfileMS.entity.Doctor;
-import com.hms.ProfileMS.entity.Patient;
 
 public interface DoctorRepository extends CrudRepository<Doctor, Long> {
     
-    Optional<Patient> findByEmail(String email);
-    Optional<Patient> findByLicenseNo(String licenseNo);
+    Optional<Doctor> findByEmail(String email);
+    Optional<Doctor> findByLicenseNo(String licenseNo);
 
 }
